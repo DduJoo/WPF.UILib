48ff0f55201531c2c7f0722148d5eda9c8a5af42
김주래 <rlawnfo5824@douzone.com>
2023-02-28 17:44:41 +0900

    DataGrid.xaml 추가

--- WPF.UILib.Demo/MainWindowModel.cs
+++ WPF.UILib.Demo/MainWindowModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Windows;
 using System.Windows.Input;
 using WPF.UILib.Controls;
@@ -20,6 +21,10 @@ namespace WPF.UILib.Demo
         private string selectedString = string.Empty;
         public string SelectedString { get => selectedString; set => SetProperty(ref selectedString, value); }
 
+        private DataTable dTable = new DataTable();
+        public DataTable DTable { get => dTable; set => SetProperty(ref dTable, value); }
+
+
 
         public MainWindowModel()
         {
@@ -27,6 +32,25 @@ namespace WPF.UILib.Demo
             {
                 ThemesList.Add(theme.ToString());
             }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("First");
+            dt.Columns.Add("Seconds");
+            dt.Columns.Add("Third");
+            dt.Columns.Add("Fourth");
+
+
+            dt.Rows.Add("1", "2", "3", "4");
+            dt.Rows.Add("11", "22", "33", "44");
+            dt.Rows.Add("111", "222", "333", "444");
+            dt.Rows.Add("1111", "2222", "3333", "4444");
+            dt.Rows.Add("11111", "22222", "33333", "44444");
+            dt.Rows.Add("1111", "2222", "3333", "4444");
+            dt.Rows.Add("111", "222", "333", "444");
+            dt.Rows.Add("11", "22", "33", "44");
+            dt.Rows.Add("1", "2", "3", "4");
+
+            DTable = dt.Copy();
         }
 
         private void Changed()
